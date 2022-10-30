75d33914d4dd64faa42f8cb1b4f5d70623bbb32c
Ken Stanley <dohpaz@gmail.com>
2022-10-30 18:28:55 -0400

    Remove unused imports

--- app/src/main/java/edu/uncc/inclass10/CreatePostFragment.java
+++ app/src/main/java/edu/uncc/inclass10/CreatePostFragment.java
@@ -17,12 +17,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import edu.uncc.inclass10.databinding.FragmentCreatePostBinding;
